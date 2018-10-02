155b10857f4f17388c33e25ca68ce6da6b8396ee
David Delabassee <david.delabassee@gmail.com>
2018-10-02 19:08:35 +0200

    uses @FnFeature annotation

--- simpleflow/src/main/java/com/example/fn/HelloFunction.java
+++ simpleflow/src/main/java/com/example/fn/HelloFunction.java
@@ -7,20 +7,18 @@ import com.fnproject.fn.api.FnFeature;
 import static com.fnproject.fn.api.Headers.emptyHeaders;
 import static com.fnproject.fn.api.flow.HttpMethod.POST;
 
-
 @FnFeature(FlowFeature.class)
-public class SimpleFlowFunction {
+public class HelloFunction {
 
     public String handleRequest(String input) {
 
         Flow flow = Flows.currentFlow();
 
-        // invokeFunction  now takes an Fn Function ID of the func to invoke  rather than `app/path`
+        // invokeFunction now takes an Fn Function ID of the func to invoke rather than `app/path`
         // `fn inspect function appName fnName`
 
-		FlowFuture<byte[]> stage = flow.invokeFunction( "01CRQVKFZDNG8G00GZJ0000001", POST,
-                emptyHeaders(),
-				input.getBytes() )
+		FlowFuture<byte[]> stage = 
+			flow.invokeFunction( "01CRTRQG6CNG8G00GZJ0000004", POST, emptyHeaders(), input.getBytes() )
                 .thenApply(HttpResponse::getBodyAsBytes);
 
         return new String(stage.get());
